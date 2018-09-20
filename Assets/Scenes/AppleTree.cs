b2d2ba0c577ff6372c126ea37f2d19b857520bc1
Aaron Rubet <arubet2940@gmail.com>
2018-09-20 15:06:48 -0400

    Apple-Picker
    almost done project baskets wont break

---
+++ Assets/Scenes/AppleTree.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+    [Header("Set in Inspector")]
+    //prefab for instantiating apples
+    public GameObject applePrefab;
+
+    //speed at which AppleTree moves
+    public float speed = 1f;
+
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    //chance that the AppleTree will Change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    //Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
+    void Start () {
+        //dropping apples every second
+        Invoke("DropApple", 2f);
+	
+	}
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+    void Update()
+    {
+        //Basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        //Change direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); // Move Right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); // Move Left
+        }
+    }
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1; //Change direction
+        }
+    }
+    }
